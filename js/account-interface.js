791a6cb9ccd5e72ea2a6c5720fdad5dcc47128a6
Stephen Newkirk <newkirk771@gmail.com>
2016-10-14 09:52:40 -0700

    Fix bug preventing form from submitting in interface. API call still untested.

--- js/account-interface.js
+++ js/account-interface.js
@@ -2,6 +2,13 @@ var Account = require("./../js/account.js").accountModule;
 
 $(document).ready(function() {
 
-
+  $('#account-form').submit(function(event) {
+    event.preventDefault();
+    var username = $("#u-name").val();
+    $("#u-name").val("");
+    var newAccount = new Account(username);
+    console.log(newAccount.username);
+    newAccount.getRepos(username);
+  });
 
 });
--- js/account.js
+++ js/account.js
@@ -1,14 +1,14 @@
 var apiKey = require("./../.env").apiKey;
 
-function Account() {
-  this.username = "";
+function Account(username) {
+  this.username = username;
 }
 
 Account.prototype.getRepos = function(username){
-  $.get('https://api.github.com/users' + username + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/:' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
-    console.log(error.responseJson.message);
+    console.log(error.responseJSON.message);
   });
 };
 
