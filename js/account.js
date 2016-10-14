ce7b9e3c0347bd4640ba2e9c6a81a16c4c982f8c
Stephen Newkirk <newkirk771@gmail.com>
2016-10-14 09:55:31 -0700

    API call working with sample code from curriculum.

--- js/account.js
+++ js/account.js
@@ -4,8 +4,10 @@ function Account(username) {
   this.username = username;
 }
 
-Account.prototype.getRepos = function(username){
-  $.get('https://api.github.com/users/:' + username + '?access_token=' + apiKey).then(function(response){
+
+
+Account.prototype.getRepos = function(){
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
